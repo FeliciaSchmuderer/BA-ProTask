1e401837ea73543b09b59eb44ee70d0ff0026ba5
Felicia Schmuderer <felicia-schmuderer@t-online.de>
2026-07-03 09:05:52 +0200

    add file toDoList_startscreen

---
+++ protask_app/lib/toDoList_startscreen/toDoList_startscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TodolistStartscreen extends StatefulWidget {
+  const TodolistStartscreen({super.key});
+
+  @override
+  State<TodolistStartscreen> createState() => _TodolistStartscreenState();
+}
+
+class _TodolistStartscreenState extends State<TodolistStartscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
