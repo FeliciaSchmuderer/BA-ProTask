1a18b9f2e362a63c4e35bb0a55fd88d1f154983e
Felicia Schmuderer <felicia-schmuderer@t-online.de>
2026-09-09 18:18:52 +0200

    change TimePicker to whell time picker

--- protask_app/lib/helpers/app_time_picker.dart
+++ protask_app/lib/helpers/app_time_picker.dart
@@ -1,14 +1,42 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AppTimePicker {
-  // opens time picker with the current selection as initial time
+  // opens wheel time picker with the current selection as initial time
   static Future<TimeOfDay?> selectTime(
     BuildContext context,
     TimeOfDay? selectedTime,
-  ) {
-    return showTimePicker(
+  ) async {
+    final initialTime = selectedTime ?? TimeOfDay.now();
+
+    TimeOfDay? pickedTime;
+
+    await showCupertinoModalPopup(
       context: context,
-      initialTime: selectedTime ?? TimeOfDay.now(),
+      builder: (context) {
+        return Container(
+          height: 216,
+          color: CupertinoColors.systemBackground.resolveFrom(context),
+          child: SafeArea(
+            top: false,
+            child: CupertinoDatePicker(
+              mode: CupertinoDatePickerMode.time,
+              use24hFormat: true,
+              initialDateTime: DateTime.now().copyWith(
+                hour: initialTime.hour,
+                minute: initialTime.minute,
+              ),
+              onDateTimeChanged: (DateTime newTime) {
+                pickedTime = TimeOfDay(
+                  hour: newTime.hour,
+                  minute: newTime.minute,
+                );
+              },
+            ),
+          ),
+        );
+      },
     );
+    return pickedTime;
   }
 }
