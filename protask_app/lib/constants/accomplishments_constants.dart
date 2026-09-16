d887d6a69cd4570c3ac80905220af9268abec4a5
Felicia Schmuderer <felicia-schmuderer@t-online.de>
2026-09-16 21:07:41 +0200

    fix accomplishment text

--- protask_app/lib/constants/accomplishments_constants.dart
+++ protask_app/lib/constants/accomplishments_constants.dart
@@ -1,6 +1,6 @@
 class AccomplishmentsConstants {
   static const String titleToday = "Today's Accomplishments";
-  static const String titleOtherDays = 'No accomplishments on this day.';
+  static const String titleOtherDays = 'Accomplishments';
   static const String hintText = "Add Accomplishment";
   static const String noAccomplishmentsText = "No accomplishments on this day.";
 }
