668f84e0c7f9646019fde7be13316882b7b2690e
Felicia Schmuderer <felicia-schmuderer@t-online.de>
2026-09-20 14:02:20 +0200

    add timer status

---
+++ protask_app/lib/todo_timer/timer_status.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:protask_app/constants/timer_constants.dart';
+import 'package:protask_app/provider/timer_provider.dart';
+
+class TimerStatus extends StatelessWidget {
+  final TodoTimerState state;
+
+  const TimerStatus({
+    super.key,
+    required this.state,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      _getStatusText(),
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: TimerConstants.statusFontSize,
+        color: TimerConstants.secondaryTextColor(
+          context,
+        ),
+      ),
+    );
+  }
+
+  String _getStatusText() {
+    switch (state) {
+      case TodoTimerState.idle:
+        return 'Ready';
+
+      case TodoTimerState.running:
+        return 'Timer is running';
+
+      case TodoTimerState.paused:
+        return 'paused';
+
+      case TodoTimerState.stopped:
+        return 'Stopped';
+
+      case TodoTimerState.finished:
+        return 'Finished';
+
+      case TodoTimerState.expired:
+        return 'Time expired';
+    }
+  }
+}
